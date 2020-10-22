0bca5a41a965f7f8f7a93f9e47b9836199bd26f3
ioanaroceanu2000 <ioanaroceanu@gmail.com>
2020-10-22 18:19:13 +0100

    change price to string

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const WETH_ADD = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 
 app.get('/ethPriceUSD', async (req, res) => {
   const price = await ethPriceUSD();
-  res.send(price);
+  res.send({priceETH: price});
 });
 
 app.get('/tokensVolumeOrder', async (req, res) => {
