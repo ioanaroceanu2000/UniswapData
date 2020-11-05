0b8bce3f0dac954a49c3d14bcab796e874f14828
Ioana Roceanu <ioanaroceanu@Ioanas-MacBook-Pro.local>
2020-11-05 17:35:12 +0000

    added Token variable and changed pairs to not just ETH

--- index.js
+++ index.js
@@ -68,6 +68,28 @@ const tokensVolumeOrder = async () => {
 
 }
 
+//top volume top 300 tokens - volumeUSD and liquidity
+const tokenVolume = async (tokenId) => {
+  try{
+    const result = await axios.post(
+      UNI_URL,
+      {
+        query: `{
+          token(id: "${tokenId}") {
+            symbol
+            tradeVolumeUSD
+          }
+        }`
+      }
+    );
+    return result.data.data.token;
+  }catch(err){
+    console.log("Error in token volume", err);
+    return 0;
+  }
+
+}
+
 //IDs of top volume top 300 tokens - volumeUSD and liquidity
 const tokensVolumeOrderIDs = async () => {
   try{
@@ -131,11 +153,23 @@ const tokenPriceUSD = async (tokenId) => {
 
 //created at timestamp
 const dateCreationPairWithEth = async (tokenId) => {
+  // array of ids for [WETH, DAI, USDC,USDT,sUSD,BUSD,TUSD]
+  const keyTokens = ["0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", "0x6b175474e89094c44da98b954eedeac495271d0f", "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48","0xdac17f958d2ee523a2206206994597c13d831ec7", "0x57ab1ec28d129707052df4df418d58a2d46d5f51", "0x4fabb145d64652a948d72533023f6e7a623c7c53","0x0000000000085d4780b73119b644ae5ecd22b376"];
+  if(keyTokens.includes(tokenId)){
+    return 1;
+  }
   try{
     //find pairs using token id
-    var pairId = getPairId(tokenId,WETH_ADD);
-    console.log(pairId)
-
+    let pairId = 0;
+    var i = 0;
+    while(i < 7 && pairId == 0){
+      pairId = getPairId(keyTokens[i],tokenId);
+      i++;
+    }
+    if(pairId == 0){
+      console.log('pair not found');
+      return 0;
+    }
     const result = await axios.post(
       UNI_URL,
       {
@@ -147,11 +181,11 @@ const dateCreationPairWithEth = async (tokenId) => {
       }
     );
     var timestamp = result.data.data.pair.createdAtTimestamp;
-    console.log(timestamp);
     var date = new Date(timestamp*1000);
-    console.log(date.toUTCString());
+    return date.toUTCString();
   }catch(error){
     console.error('Error in date creation', error);
+    return 0;
   }
 }
 
@@ -159,6 +193,40 @@ const dateCreationPairWithEth = async (tokenId) => {
 
 
 //JSON REGISTERS
+
+// initialise tokens variables for interest rates
+async function recordTokensVariables(){
+  console.log('try records tokens variables');
+  let pricesRecords = JSON.parse(fs.readFileSync('Prices.json'));
+  let obj = {}; //token id, volume, std, longevity
+  for(i = 0; i < 300; i++){
+    let obj2 = {};
+    obj2.id = pricesRecords[i].id;
+    obj2.volatility = pricesRecords[i].std;
+    obj2.volume = 0;
+    obj2.symbol = 0;
+    obj2.creationDate = 0;
+    let tokenVolumeData = await tokenVolume(obj2.id);
+    let tokenLongevity = await dateCreationPairWithEth(obj2.id);
+    if(tokenVolumeData != 0 && tokenLongevity != 0){
+      obj2.volume = tokenVolumeData.tradeVolumeUSD;
+      obj2.symbol = tokenVolumeData.symbol;
+      obj2.creationDate = tokenLongevity;
+    }else{
+      console.log('failed to get token volume');
+    }
+    obj[i] = obj2;
+    console.log(i);
+  }
+
+  if(obj != 0){
+    var json = JSON.stringify(obj);
+    fs.writeFile('TokensVariables.json', json, (err) => {
+        // In case of a error throw err.
+        if (err) throw err;
+    });
+  }
+}
 
 //register token's price
 async function registerPrice(){
@@ -188,7 +256,8 @@ async function registerPrice(){
   json = JSON.stringify(pricesRecords); //convert it back to json
   fs.writeFileSync('Prices.json', json) // write it back
 }
-setInterval(registerPrice, 15000); //3600000 one hour
+//setInterval(registerPrice, 15000); //3600000 one hour
+
 //initialise Price Records to 0
 /*async function initialisePriceRegister(){
   //tokenPriceNow = await tokenPriceUSD("0x1f9840a85d5af5bf1d1762f925bdaddc4201f984");
@@ -245,16 +314,20 @@ async function recordTokensVolumeOrder(){
     });
   }
 }
-setInterval(recordTokensVolumeOrder, 17000); //every minute
+//setInterval(recordTokensVolumeOrder, 17000); //every minute
 
 //get pair id from two tokens addresses
 function getPairId(token0, token1){
   try {
-    const jsonString = fs.readFileSync('./Output.json')
-    const pairsJson = JSON.parse(jsonString)
-    return pairsJson["pairs"][token0][token1];
+    const jsonString = fs.readFileSync('./Output.json');
+    const pairsJson = JSON.parse(jsonString);
+    if(pairsJson["pairs"][token0][token1] != undefined){
+      return pairsJson["pairs"][token0][token1];
+    }else {
+      return 0;
+    }
   } catch(err) {
-    console.log('Error in getting the pair id', err)
-    return
+    console.log('Error in getting the pair id', err);
+    return 0;
   }
 }
--- pairs.js
+++ pairs.js
@@ -2,6 +2,9 @@ const axios = require('axios');
 const fs = require('fs') ;
 const UNI_URL = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2';
 
+// array of ids for [WETH, DAI, USDC,USDT,sUSD,BUSD,TUSD]
+const keyTokens = ["0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", "0x6b175474e89094c44da98b954eedeac495271d0f", "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48","0xdac17f958d2ee523a2206206994597c13d831ec7", "0x57ab1ec28d129707052df4df418d58a2d46d5f51", "0x4fabb145d64652a948d72533023f6e7a623c7c53","0x0000000000085d4780b73119b644ae5ecd22b376"];
+
 function mapToObj(map){
   var obj = {};
   map.forEach(function(v, k){
@@ -24,8 +27,8 @@ function simpleMapToObj(map){
 const computeAllPairs = async () => {
   try{
     let pairsMap = new Map();
-    const increment = 100;
-    for(i=0; i < 1600; i+=increment){
+    const increment = 1000;
+    for(i=0; i < 5001; i+=increment){
       const result = await axios.post(
         UNI_URL,
         {
@@ -50,14 +53,19 @@ const computeAllPairs = async () => {
         var token0Id = array[j].token0.id;
         var token1Id = array[j].token1.id;
         var pairId = array[j].id;
-        if(pairsMap.has(token0Id) == false){
-          pairsMap.set(token0Id, new Map());
+        if(keyTokens.includes(token0Id)){
+          if(pairsMap.has(token0Id) == false){
+            pairsMap.set(token0Id, new Map());
+          }
+          pairsMap.get(token0Id).set(token1Id, pairId);
         }
-        if(pairsMap.has(token1Id) == false){
-          pairsMap.set(token1Id, new Map());
+        if(keyTokens.includes(token1Id)){
+          if(pairsMap.has(token1Id) == false){
+            pairsMap.set(token1Id, new Map());
+          }
+          pairsMap.get(token1Id).set(token0Id, pairId);
         }
-        pairsMap.get(token0Id).set(token1Id, pairId);
-        pairsMap.get(token1Id).set(token0Id, pairId);
+
       }
     }
 
